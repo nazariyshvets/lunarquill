bba0074935a3f6033696db8c50dde2d2d7af26ab
Nazarii Shvets <nazar123005@gmail.com>
2024-01-16 14:51:14 +0200

    add full screen for featured user

--- client/src/components/FeaturedUser.tsx
+++ client/src/components/FeaturedUser.tsx
@@ -1,11 +1,62 @@
-import { PropsWithChildren } from "react";
+import { useState, useRef, useEffect, PropsWithChildren } from "react";
+import { BiFullscreen, BiExitFullscreen } from "react-icons/bi";
+import RTCControlButton from "./RTCControlButton";
 
 const FeaturedUser = ({
   children,
 }: PropsWithChildren<Record<never, never>>) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const fullScreenElementRef = useRef<HTMLDivElement>(null);
+
+  const toggleFullScreen = async () => {
+    try {
+      if (isFullScreen) {
+        await document.exitFullscreen();
+        setIsFullScreen(false);
+      } else {
+        await fullScreenElementRef.current?.requestFullscreen();
+        setIsFullScreen(true);
+      }
+    } catch (err) {
+      setIsFullScreen(false);
+      console.log(err);
+    }
+  };
+
+  // Exit full screen mode when a user clicks the "Esc" button
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      if (!document.fullscreenElement) {
+        setIsFullScreen(false);
+      }
+    };
+
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
+
   return (
-    <div className="h-full w-full overflow-hidden rounded-lg bg-deep-black">
+    <div
+      ref={fullScreenElementRef}
+      className={`relative h-full w-full overflow-hidden bg-deep-black ${
+        !isFullScreen ? "rounded-lg" : ""
+      }`}
+    >
       {children}
+
+      <RTCControlButton
+        className="absolute bottom-2 right-2 z-10"
+        onClick={toggleFullScreen}
+      >
+        {isFullScreen ? (
+          <BiExitFullscreen className="h-full w-full text-primary" />
+        ) : (
+          <BiFullscreen className="h-full w-full text-white" />
+        )}
+      </RTCControlButton>
     </div>
   );
 };
--- client/src/components/RTCControlButton.tsx
+++ client/src/components/RTCControlButton.tsx
@@ -2,12 +2,13 @@ import { PropsWithChildren, ButtonHTMLAttributes } from "react";
 
 const RTCControlButton = ({
   children,
+  className,
   onClick,
   ...rest
 }: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
   return (
     <button
-      className="h-12 w-12 rounded-full p-3 transition-shadow hover:shadow-button sm:h-14 sm:w-14"
+      className={`h-12 w-12 rounded-full p-3 transition-shadow hover:shadow-button sm:h-14 sm:w-14 ${className}`}
       onClick={onClick}
       {...rest}
     >
